0059a4e953e053d714790a1f50ad79575685c83a
aksolotol <yahontdydnikovz228@gmail.com>
2026-01-11 03:43:57 +0300

    v1.1 design update

--- ExperienceCalculator.cs
+++ ExperienceCalculator.cs
@@ -14,7 +14,7 @@ namespace caculator
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-
+            
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
